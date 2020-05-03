5ea82c237ddcc6f11f3e01cf2d6d1ffb514cb82e
Guillaume Kinif <guillaume.kinif@collibra.com>
2020-05-03 20:25:43 +0200

    set token for socket

--- lib/portfolio_web/plugs/request_authenticated.ex
+++ lib/portfolio_web/plugs/request_authenticated.ex
@@ -1,5 +1,5 @@
 defmodule PortfolioWeb.Plugs.RequestAuthenticated do
-  import Plug.Conn, only: [get_session: 2, halt: 1]
+  import Plug.Conn, only: [get_session: 2, halt: 1, assign: 3]
   import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
 
   def init(opts), do: opts
@@ -8,12 +8,15 @@ defmodule PortfolioWeb.Plugs.RequestAuthenticated do
     case get_session(conn, :user_id) do
       nil ->
         conn
+        |> assign(:user_token, nil)
         |> put_flash(:error, "You must be logged in to execute this action!")
         |> redirect(to: "/")
         |> halt()
 
-      _ ->
+      user_id ->
+        token = Phoenix.Token.sign(conn, "user socket", user_id)
         conn
+        |> assign(:user_token, token)
     end
   end
 end
