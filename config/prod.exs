118facc5c0e17b6d1c73978335bb4e89871d4520
Guillaume Kinif <guillaume.kinif@collibra.com>
2020-05-03 20:02:17 +0200

    Try moving back to prod.secret.exs

--- config/prod.exs
+++ config/prod.exs
@@ -53,4 +53,4 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 # No longer needed because we use elixir release
-# import_config "prod.secret.exs"
+import_config "prod.secret.exs"
