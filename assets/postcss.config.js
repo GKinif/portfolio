4c812f931a46cbe2e2a68691d65f011c9c1a77f3
Guillaume Kinif <guillaume.kinif@collibra.com>
2020-05-03 22:05:07 +0200

    Fix tailwindcss custom color

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -14,7 +14,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    // require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production'
