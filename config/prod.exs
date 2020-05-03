e47f8728df7793904e55716cf927d8bc890ca3aa
Guillaume Kinif <guillaume.kinif@collibra.com>
2020-05-03 20:46:40 +0200

    Fix check_origin in endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :portfolio, PortfolioWeb.Endpoint,
   url: [scheme: "https", host: "guillaumekinif.be", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://www.guillaumekinif.be", "https://guillaumekinif.be", "//guillaumekinif.be"]
 
 # Do not print debug messages in production
 config :logger, level: :info
