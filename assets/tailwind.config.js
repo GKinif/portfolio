5c694f859c5146b7945a74b9b4357c6aeb399d85
Guillaume Kinif <guillaume.kinif@collibra.com>
2020-05-10 19:19:23 +0200

    fix user dropdown and admin layout

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,6 +13,21 @@ module.exports = {
       '0': 0,
       '1/2': '50%',
     },
+    zIndex: {
+      '0': 0,
+      '10': 10,
+      '20': 20,
+      '30': 30,
+      '40': 40,
+      '50': 50,
+      '60': 60,
+      '70': 70,
+      '80': 80,
+      '90': 90,
+      '100': 100,
+      '1000': 1000,
+      'auto': 'auto',
+    },
     // https://javisperez.github.io/tailwindcolorshades/#/
     extend: {
       colors: {
